e905bb7d8c88cf0ed31fe87adf04c44a0ad44f6e
Penghk <1769476788@qq.com>
2022-01-27 16:18:38 +0800

    文件名称修改

--- pages/find/index.js
+++ pages/find/index.js
@@ -5,43 +5,43 @@ Page({
       // 图标来源：https://www.iconfont.cn/collections/detail?spm=a313x.7781069.1998910419.dc64b3430&cid=32622
       [
         {
-          icon: "/assets/img/message/position.png",
+          icon: "/assets/img/find/position.png",
           txt: "附近的人"
         },
         {
-          icon: "/assets/img/message/group.png",
+          icon: "/assets/img/find/group.png",
           txt: "发现群组"
         }
       ],
 
       [
         {
-          icon: "/assets/img/message/live.png",
+          icon: "/assets/img/find/live.png",
           txt: "直播间"
         }
       ],
 
       [
         {
-          icon: "/assets/img/message/sign.png",
+          icon: "/assets/img/find/sign.png",
           txt: "每日签到",
           rightTxt: "未签到"
         },
         {
-          icon: "/assets/img/message/task.png",
+          icon: "/assets/img/find/task.png",
           txt: "任务中心",
           rightTxt: "完成度12.5%"
         },
         {
-          icon: "/assets/img/message/rank.png",
+          icon: "/assets/img/find/rank.png",
           txt: "排行榜"
         },
         {
-          icon: "/assets/img/message/shop.png",
+          icon: "/assets/img/find/shop.png",
           txt: "商城"
         },
         {
-          icon: "/assets/img/message/hot.png",
+          icon: "/assets/img/find/hot.png",
           txt: "最热帖排行"
         }
       ]
