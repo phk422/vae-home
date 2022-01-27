4b496e58d6646f6638ebaa52f5fe38792896d9a2
Penghk <1769476788@qq.com>
2022-01-27 11:04:49 +0800

    抽取与封装menu-group组件

---
+++ components/menu-group/index.js
@@ -0,0 +1,35 @@
+// components/menu-group/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    menus: {
+      type: Array,
+      value: [
+        [
+          {
+            txt: "附近的人"
+          },
+          {
+            txt: "发现群组"
+          }
+        ]
+      ]
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
