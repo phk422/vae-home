51af7289587489d831d0d30ac961ceb465466c95
Penghk <1769476788@qq.com>
2022-01-27 16:23:04 +0800

    消息界面图标

--- pages/message/index.js
+++ pages/message/index.js
@@ -4,13 +4,29 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 图标来源：https://www.iconfont.cn/collections/detail?spm=a313x.7781069.1998910419.dc64b3430&cid=23763
     menus: [
-      [
-        {txt: "官方消息"},
-        {txt: "@我的"},
-        {txt: "评论"},
-        {txt: "赞"},
-        {txt: "陌生人", rightTxt: "2021/12/13"},
+      [{
+          icon: "/assets/img/message/computer.png",
+          txt: "官方消息"
+        },
+        {
+          icon: "/assets/img/message/draw.png",
+          txt: "@我的"
+        },
+        {
+          icon: "/assets/img/message/mountain.png",
+          txt: "评论"
+        },
+        {
+          icon: "/assets/img/message/phone.png",
+          txt: "赞"
+        },
+        {
+          icon: "/assets/img/message/swim.png",
+          txt: "陌生人",
+          rightTxt: "2021/12/13"
+        },
       ]
     ]
   },
