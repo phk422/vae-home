830ca7993657bfe53a4d859afc9eade1ab3d8e81
Penghk <1769476788@qq.com>
2022-02-08 19:37:39 +0800

    圈子-新增图标

--- pages/circle/square/cpns/menus/index.js
+++ pages/circle/square/cpns/menus/index.js
@@ -10,52 +10,52 @@ Component({
         [
           {
             txt: "呼吸之野",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/1.png"
           },{
             txt: "留香",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/2.png"
           },{
             txt: "2022",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/3.png"
           },{
             txt: "有关V",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/4.png"
           },{
             txt: "原创圈",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/5.png"
           },{
             txt: "语音圈",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/6.png"
           },{
             txt: "露个脸",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/7.png"
           },{
             txt: "想知道",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/8.png"
           }
         ],
         [
           {
             txt: "生活圈",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/9.png"
           },{
             txt: "嘿圈粉",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/10.png"
           },{
             txt: "哈哈哈",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/11.png"
           },{
             txt: "松果圈",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/12.png"
           },{
             txt: "公告圈",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/13.png"
           },{
             txt: "游戏圈",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/14.png"
           },{
             txt: "灌水圈",
-            icon: "/assets/img/opr-img/trip.png"
+            icon: "/assets/img/circle/15.png"
           },
         ]
       ]
